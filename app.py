b82fb1686f722c3921cf221ac12724ea252cce2a
Saile5234 <elias.ortiz0523@gmail.com>
2026-04-02 12:06:24 -0500

    Added test file

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+st.title('Insert Title Here')
